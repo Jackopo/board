ce3abb48594b6ceb11b77d5a51bbb496d50c2a8b
Jacopo Ghisolfi <jacopo.ghisolfi@gmail.com>
2014-02-22 16:37:22 +0100

    Started the implement the mysql connection  to the db

--- chess/ChessConnector.java
+++ chess/ChessConnector.java
@@ -1,23 +1,59 @@
 package de.htw.ds.board.chess;
 
-public class ChessConnector implements AutoCloseable {
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
-	public ChessConnector() {
-		// TODO Auto-generated constructor stub
+import javax.sql.DataSource;
+
+import de.htw.ds.shop.ShopConnector;
+import de.sb.javase.sql.JdbcConnectionMonitor;
+
+public class ChessConnector implements AutoCloseable {
+	
+	static private final String SQL_INSERT_MOVE = "INSERT INTO chess.openingMove VALUES (0, ?, ?, ?, ?, ?)";
+	static private final String SQL_SELECT_MOVES = "SELECT * FROM chess.openingMove where position=? AND source=?  AND sink=? AND rating=? and searchDepth=?";
+	private final Connection connection;
+	static private final DataSource DATA_SOURCE;
+	static {
+		try {
+			final Properties properties = new Properties();
+			try (InputStream byteSource = ChessConnector.class.getResourceAsStream("chess-mysql.properties")) {
+				properties.load(byteSource);
+			}
+
+			final Class<?> dataSourceClass = Class.forName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource", true, Thread.currentThread().getContextClassLoader());
+			final DataSource dataSource = (DataSource) dataSourceClass.newInstance();
+			dataSourceClass.getMethod("setURL", String.class).invoke(dataSource, properties.get("connectionUrl"));
+			dataSourceClass.getMethod("setCharacterEncoding", String.class).invoke(dataSource, properties.get("characterEncoding"));
+			dataSourceClass.getMethod("setUser", String.class).invoke(dataSource, properties.get("alias"));
+			dataSourceClass.getMethod("setPassword", String.class).invoke(dataSource, properties.get("password"));
+			DATA_SOURCE = dataSource;
+		} catch (final Exception exception) {
+			throw new ExceptionInInitializerError(exception);
+		}
+	}
+	
+	
+	public ChessConnector() throws SQLException {
+		this.connection = DATA_SOURCE.getConnection();
+
+		final Runnable connectionMonitor = new JdbcConnectionMonitor(this.connection, "select null", 60000);
+		final Thread thread = new Thread(connectionMonitor, "jdbc-connection-monitor");
+		thread.setDaemon(true);
+		thread.start();
 	}
 
 	@Override
 	public void close() throws Exception {
-		// TODO Auto-generated method stub
+		this.connection.close();
 
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public boolean createDatabse() {
+		
+		return false;
 	}
 
 }

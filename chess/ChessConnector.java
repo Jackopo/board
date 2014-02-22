05ce042e3d79209fdd2cd46f500df403964dbe1a
Jacopo Ghisolfi <jacopo.ghisolfi@gmail.com>
2014-02-22 15:58:54 +0100

    Added the ChessConnector class

---
+++ chess/ChessConnector.java
@@ -0,0 +1,23 @@
+package de.htw.ds.board.chess;
+
+public class ChessConnector implements AutoCloseable {
+
+	public ChessConnector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
